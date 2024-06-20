a1813d48b62cbe7f6c871f2f0b689ff679763fbe
evanjelio <s22210700@student.unklab.ac.id>
2024-06-20 09:20:02 +0800

    adding pertemuan

---
+++ pertemuan-11/index.js
@@ -0,0 +1,21 @@
+
+// import helloWorld from './helloWorld';
+
+// async function messages() {
+//   const msg = await helloWorld();
+//   console.log(msg);
+// }
+
+// messages();
+
+
+
+// import ambilDataUser from './ambilDataUser';
+
+// ambilDataUser();
+
+
+
+// import ambilDataUserAsync from './ambilDataUserAsync';
+
+// ambilDataUserAsync();
---
+++ pertemuan-8/exe5.js
@@ -0,0 +1,48 @@
+//Evanjelio kombaitan
+//Ubahlah syntax ES5 berikut ke dalam ES6
+
+// //1.
+// const calculateAge = (birthYear) => 2019 - birthYear;
+
+// const yearUntilRetirement = (object) => {
+//     const age = calculateAge(object.year);
+//     const retirement = 60 - age;
+
+//     if (retirement > 0) {
+//         console.log(`${object.firstName} retires in ${retirement} years`);
+//     } else {
+//         console.log(`${object.firstName} is already retired.`);
+//     }
+// };
+
+// //2.
+// const addNumber = (a, b, c, d, e, f, g) => {
+//     const numbers = [a, b, c, d, e, f, g];
+//     let sum = 0;
+//     numbers.map(number => sum += number);
+//     return sum;
+// }
+
+// console.log(addNumber(1, 2, 3, 4, 5, 6, 7));
+
+// //3.
+// const phi = 3.14;
+// const power = 2;
+// let radius = 0;
+
+// const calculateArea = (obj) => phi * Math.pow(obj.radius, obj.power);
+
+// radius = 21;
+// const area21 = calculateArea({ radius, power: 2 });
+
+// radius = 7;
+// const area7 = calculateArea({ radius, power: 2 });
+
+// console.log(`area with 21 radius: ${area21}, and area with 7 radius: ${area7}`);
+
+// // 4.
+// const makeAjaxRequest = (url, method = 'GET') => {
+//     console.log(url, method);
+// };
+
+// makeAjaxRequest('www.google.com');
